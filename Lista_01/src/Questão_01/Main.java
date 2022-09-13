5d877f56617946b5c2dfadc49f3e55db8ccbf917
Gilson filho <101582105+Gilsonfilho1@users.noreply.github.com>
2022-09-12 23:32:25 -0300

    Add files via upload

---
+++ Lista_01/src/Questão_01/Main.java
@@ -0,0 +1,18 @@
+package Questão_01;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		
+		System.out.println("Digite um numero: ");
+		num = sc.nextInt();
+		System.out.println("numero digitado: "+ num);
+		
+
+	}
+
+}
