328ca16486c1450b38a61f53198c13793f50c600
Gilson filho <101582105+Gilsonfilho1@users.noreply.github.com>
2022-09-13 10:16:48 -0300

    Add files via upload

---
+++ Lista_01/src/Questão_02/Main.java
@@ -0,0 +1,16 @@
+package Questão_02;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float num;
+		System.out.println("Digite um numero real: ");
+		num = sc.nextFloat();
+		System.out.println("Numero digitado: " + num);
+
+	}
+
+}
